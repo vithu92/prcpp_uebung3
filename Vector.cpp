31e4369630dbd1fea815a21e27f14ee0b1a3edac
vithu92 <vithushan.mahendran@students.fhnw.ch>
2018-12-01 00:56:57 +0100

    further fixing problems

--- Vector.cpp
+++ Vector.cpp
@@ -1,10 +1,22 @@
 #include "vector.h"
+#include <iostream>
+#include <memory>
 
 using namespace std;
 
 int main() {
 
+	Vector<double> v1(5, 1.9);
+	cout << v1 << endl;
+
+	Vector<int> v2{ 4,3,2,1,2 };
+	Vector<int> v3{ 4,3,2,1,2 };
+	cout << v2 << endl;
 	
+	cout << v2*v3 << endl;
+
+	
+
 	
 
 }
--- vector.h
+++ vector.h
@@ -7,47 +7,59 @@
 using namespace std;
 
 template<class T> class Vector {
+
 	unique_ptr<T[]> m_array;
 
-	//Anzahl der Elemente im Array m_array
-	size_t m_size;
-	//Die Länge des Arrays m_array
-	size_t m_capacity:
-
+	size_t m_capacity; //die Länge des Arrays m_array
+	size_t m_size; //die Anzahl Elemente im Array m_array
 
 public:
 
-	//Konstruktor mit Dimensionsgrösse und -werte
+	Vector(size_t capacity = 10)
+		: m_array(new T[capacity]), m_capacity(capacity), m_size(0) {};
+
+	//Konstruktor mit Vektor als Parameter
+	Vector(const Vector& v)
+		: m_array(new T[v.m_capacity]), m_capacity(v.m_capacity), m_size(v.m_size) {
+		for (size_t i = 0; i < m_size; i++) {
+			m_array[i] = v[i];
+		}
+	}
+
+	//TODO
+	Vector(Vector&& v) : m_array(move(v.m_array)), m_capacity(v.m_capacity), m_size(v.m_size) {
+		v.m_size = v.m_capacity = 0;
+	}
+
+	//Konstruktor mit Dimensionsgrösse und jeder Dimensionswert denselben Wert
 	Vector(size_t size, const T& val)
-		:m_array(new T[size]), m_capacity(size), m_size(size){
+		: m_array(new T[size]), m_capacity(size), m_size(size) {
 		for (size_t i = 0; i < m_size; i++) m_array[i] = val;
 	}
 
-	//Konstruktur mit Dimensionsgrösse
-	Vector(size_t capacity = 20) : m_array(new T[capacity]), m_capacity(capacity), m_size(0) {};
+
 
 	//Konstruktur mit Iniitialisierungsliste (Dimensionswerte)
-	Vector(const initializer_list<T>& data){
-		size_t s = __min(data.size(), m_capacity);
-
-
+	Vector(const initializer_list<T>& data): m_array(new T[data.size()]), m_capacity(data.size()), m_size(data.size()){
+				
 		auto it = data.begin();
 		for (size_t i = 0; i < m_capacity; i++) {
-			this->at(i) = *it;
+			m_array[i] = *it;
+			it++;
 		}
 	}
 	
 	//Addition Operator
 	Vector operator+(const Vector& b) {
 		Vector sum = new Vector(b.m_size);
-		if (this->m_size == b.m_size) {
-			for (size_t i = 0; i < this->m_size; i++) {
-				sum.m_array[i] = this->m_array[i]+ b.m_array[i]
+		if (this.m_size == b.m_size) {
+			for (size_t i = 0; i < this.m_size; i++) {
+				sum.m_array[i] = this.m_array[i] + b.m_array[i];
 			}
 		}
 		else
 		{
-			throw "Die Vektoren haben unterschiedliche Dimensionen!" 
+			throw "Die Vektoren haben unterschiedliche Dimensionen!";
 		}
 
 		return sum;
@@ -58,30 +70,30 @@ public:
 	Vector operator-(const Vector& b) {
 		Vector subtraktion = new Vector(b.m_size);
 		
-		if (this->m_size == b.m_size) {
-			for (size_t i = 0; i < this->m_size; i++) {
-				sum.m_array[i] = this->m_array[i] - b.m_array[i]
+		if (this.m_size == b.m_size) {
+			for (size_t i = 0; i < this.m_size; i++) {
+				subtraktion.m_array[i] = this.m_array[i] - b.m_array[i];
 			}
 		}
 		else
 		{
-			throw "Die Vektoren haben unterschiedliche Dimensionen!"
+			throw "Die Vektoren haben unterschiedliche Dimensionen!";
 		}
 		return subtraktion;
 	}
 
 	//Multiplikation Operator
-	Vector operator*(const Vector& b) {
+	Vector operator* (const Vector& b) {
 		Vector produkt = new Vector(b.m_size);
 
-		if (this->m_size == b.m_size) {
-			for (size_t i = 0; i < this->m_size; i++) {
-				sum.m_array[i] = this->m_array[i] * b.m_array[i]
+		if (this.m_size == b.m_size) {
+			for (size_t i = 0; i < this.m_size; i++) {
+				produkt.m_array[i] = this.m_array[i] * b.m_array[i];
 			}
 		}
 		else
 		{
-			throw "Die Vektoren haben unterschiedliche Dimensionen!"
+			throw "Die Vektoren haben unterschiedliche Dimensionen!";
 		}
 		return produkt;
 	}
@@ -90,29 +102,38 @@ public:
 	Vector operator/(const Vector& b) {
 		Vector division = new Vector(b.m_size);
 
-		if (this->m_size == b.m_size) {
-			for (size_t i = 0; i < this->m_size; i++) {
+		if (this.m_size == b.m_size) {
+			for (size_t i = 0; i < this.m_size; i++) {
 				if(b.m_array[i] != 0){
-				sum.m_array[i] = this->m_array[i] * b.m_array[i]
-				}else{ throw "Division nicht möglich, da man nicht durch 0 teilen kann" }
+					division.m_array[i] = this.m_array[i] * b.m_array[i];
+				}
+				else{
+					throw "Division nicht möglich, da man nicht durch 0 teilen kann";
+				}
 			}
 		}
 		else
 		{
-			throw "Die Vektoren haben unterschiedliche Dimensionen!"
+			throw "Die Vektoren haben unterschiedliche Dimensionen!";
 		}
-		return produkt;
+		return division;
+	}
+
+	//Index Operator (nur Lesezugriff)
+	const T& operator[] (size_t i) const {
+		if (i >= m_size) throw out_of_range("index is too large");
+		return m_array[i];
 	}
 
 	//Addition mit Skalar
 	Vector SkalarAddition(T skalar) {
 		
 		//abfragen nach dem Typ des Arrays
-		auto ArrayType = remove_all_extents<decltype(this->m_array)>::type;
+		auto ArrayType = remove_all_extents<decltype(this.m_array)>::type;
 
 
-		for (size_t i = 0; i < this->m_size; i++) {
-			this->m_array[i] = this->m_array[i] + (ArrayType)skalar;
+		for (size_t i = 0; i < this.m_size; i++) {
+			this.m_array[i] = this.m_array[i] + skalar;
 		}
 		return this;
 	}
@@ -121,11 +142,11 @@ public:
 	Vector SkalarSubtraktionLinks(T skalar) {
 
 		//abfragen nach dem Typ des Arrays
-		auto ArrayType = remove_all_extents<decltype(this->m_array)>::type;
+		auto ArrayType = remove_all_extents<decltype(this.m_array)>::type;
 
 
-		for (size_t i = 0; i < this->m_size; i++) {
-			this->m_array[i] = this->m_array[i] - (ArrayType)skalar;
+		for (size_t i = 0; i < this.m_size; i++) {
+			this.m_array[i] = this.m_array[i] - skalar;
 		}
 		return this;
 	}
@@ -134,24 +155,24 @@ public:
 	Vector SkalarSubtraktionRechts(T skalar) {
 
 		//abfragen nach dem Typ des Arrays
-		auto ArrayType = remove_all_extents<decltype(this->m_array)>::type;
+		auto ArrayType = remove_all_extents<decltype(this.m_array)>::type;
 
 
-		for (size_t i = 0; i < this->m_size; i++) {
-			this->m_array[i] = (ArrayType)skalar - this->m_array[i];
+		for (size_t i = 0; i < this.m_size; i++) {
+			this.m_array[i] = skalar - this.m_array[i];
 		}
 		return this;
 	}
 
 	//Multiplikation mit Skalar
-	Vector SkalarAddition(T skalar) {
+	Vector SkalarMultiplikation(T skalar) {
 
 		//abfragen nach dem Typ des Arrays
-		auto ArrayType = remove_all_extents<decltype(this->m_array)>::type;
+		auto ArrayType = remove_all_extents<decltype(this.m_array)>::type;
 
 
-		for (size_t i = 0; i < this->m_size; i++) {
-			this->m_array[i] = this->m_array[i] * (ArrayType)skalar;
+		for (size_t i = 0; i < this.m_size; i++) {
+			this.m_array[i] = this.m_array[i] * skalar;
 		}
 		return this;
 	}
@@ -160,25 +181,25 @@ public:
 	Vector SkalarDivisionRechts(T skalar) {
 
 		//abfragen nach dem Typ des Arrays
-		auto ArrayType = remove_all_extents<decltype(this->m_array)>::type;
+		auto ArrayType = remove_all_extents<decltype(this.m_array)>::type;
 
-		if((ArrayType) skalar != 0){
-			for (size_t i = 0; i < this->m_size; i++) {
-				this->m_array[i] = this->m_array[i] / (ArrayType)skalar;
+		if(skalar != 0){
+			for (size_t i = 0; i < this.m_size; i++) {
+				this.m_array[i] = this.m_array[i] / skalar;
 			}
 		}
 		return this;
 	}
 
 	//Divison mit Skalar als Dividend
-	Vector SkalarAddition(T skalar) {
+	Vector SkalarDivisonLinks(T skalar) {
 
 		//abfragen nach dem Typ des Arrays
-		auto ArrayType = remove_all_extents<decltype(this->m_array)>::type;
+		auto ArrayType = remove_all_extents<decltype(this.m_array)>::type;
 
 
-		for (size_t i = 0; i < this->m_size; i++) {
-			if(this->m_array[i] != 0) this->m_array[i] = (ArrayType)skalar / this->m_array[i];
+		for (size_t i = 0; i < this.m_size; i++) {
+			if(this.m_array[i] != 0) this.m_array[i] = skalar / this.m_array[i];
 		}
 		return this;
 	}
@@ -187,10 +208,21 @@ public:
 	T Skalarprodukt(const Vector& b) {
 		T skalarprodukt = new Vector(b.m_size);
 
-		for (size_t i = 0; i < this->m_size; i++) {
-			skalarprodukt.m_array[i] = this->m_array[i] * b.m_array[i];
+		for (size_t i = 0; i < this.m_size; i++) {
+			skalarprodukt.m_array[i] = this.m_array[i] * b.m_array[i];
 		}
 		return skalarprodukt;
 	}
 
+	friend ostream& operator<<(ostream& os, const Vector& v) {
+		os << '[';
+		if (v.m_size > 0) os << v[0];
+		for (size_t i = 1; i < v.m_size; i++) {
+			os << ',' << v[i];
+		}
+
+		return os << ']';
+	}
+
+	
 };
